6c6d2d9223df9983f1d6457360ff90e1a37cf64f
EBA <official-eba@outlook.com>
2023-10-06 10:57:45 -0400

    Tickets

---
+++ formSubmit.js
@@ -0,0 +1,31 @@
+function submitForm(name, form) {
+	formGood = true
+	formErr = ''
+	switch (name) {
+			case 'contactInfo':
+					formErr = 'All fields must be filled. Please check:'
+					if (form.firstName.value == '') { formGood = false; formErr += ' First Name;' }
+					if (form.lastName.value == '') { formGood = false; formErr += ' Last Name;' }
+					if (form.email.value == '') { formGood = false; formErr += ' Email Address;' }
+					if (form.phone.value == '') { formGood = false; formErr += ' Phone Number;' }
+					if (formGood) {
+							document.location = 'payment.html?name=' + form.firstName.value + '%20' + form.lastName.value
+					} else {
+				document.getElementById('errForm').innerHTML = formErr
+					}
+					break
+			
+			case 'buyTicket':
+					formErr = 'All fields must be filled. Please check:'
+					if (form.cardholder.value == '') { formGood = false; formErr += ' Cardholder Name;' }
+					if (form.cardnum.value == '') { formGood = false; formErr += ' Card Number;' }
+					if (form.expiry.value == '') { formGood = false; formErr += ' Expiration Date;' }
+					if (form.security.value == '') { formGood = false; formErr += ' Security Code;' }
+					if (formGood) {
+							document.location = 'success.html?name=' + form.cardholder.value
+					} else {
+				document.getElementById('errForm').innerHTML = formErr
+					}
+					break
+	}
+}
